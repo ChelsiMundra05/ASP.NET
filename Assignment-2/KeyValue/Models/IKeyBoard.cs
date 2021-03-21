df3f85d06d3223cb63c595de79fcd7ff03b66034
TARUN <chelsimaheswari17@gmail.com>
2021-03-21 18:46:55 +0530

    Assignment-2 submitted.

---
+++ Assignment-2/KeyValue/Models/IKeyBoard.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace KeyValue.Models
+{
+    public interface IKeyBoard
+    {
+        IEnumerable<Key> GetMessage();
+        Key GetById(int key);
+        Key Add(Key newMessage);
+        Key Update(Key updatedMessage);
+    }
+}
---
+++ Assignment-2/KeyValue/Models/KeyBoard.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KeyValue.Models
+{
+    public class KeyBoard : IKeyBoard
+    {
+        private readonly KeyContext db;
+
+        public KeyBoard(KeyContext db)
+        {
+            this.db = db;
+        }
+
+        public Key Add(Key newMessage)
+        {
+            try
+            {
+                db.Add(newMessage);
+                db.SaveChanges();
+                return newMessage;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public Key GetById(int key)
+        {
+            return db.Keys.Find(key);
+        }
+
+        public IEnumerable<Key> GetMessage()
+        {
+            try
+            {
+                var query = from m in db.Keys
+                            orderby m.key
+                            select m;
+                return query;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public Key Update(Key updatedMessage)
+        {
+            var entity = db.Keys.Attach(updatedMessage);   //tracks changes for the Message object that's already in there
+            entity.State = EntityState.Modified;
+            return updatedMessage;
+        }
+    }
+}
